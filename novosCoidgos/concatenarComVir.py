021f95344d78e7286b4347473ef4d047058d8814
AkiraTst <anacarolina.barros002@hotmail.com>
2025-09-18 01:15:20 +0000

    Adicionando novo exercicio

---
+++ novosCoidgos/concatenarComVir.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+escola = input("Digite o nome da sua escola: ")
+serie = int(input("Digite sua série: "))
+
+print("Olá,", nome, "! Você estuda na escola",escola,"e está na", serie,"° série.")
