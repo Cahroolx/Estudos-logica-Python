136cceae53b5b1f6b1cb505bda6d669ccfe8af75
Cahroolx <anacarolina.barros009@gmail.com>
2025-09-18 00:36:20 +0000

    Adcionando um exercício

---
+++ novosCoidgos/tiposVariaveis.py
@@ -0,0 +1,10 @@
+## Neste algoritmo, mostra os tipos de váriaveis dentro do Python
+nome = "Carol"
+altura = 1.65
+idade = 18
+estudante = True
+
+print("Tipo de váriavel do nome: ", type(nome))
+print("Tipo de váriavel da altura: ", type(altura))
+print("Tipo de váriavel do idade: ", type(idade))
+print("Tipo de váriavel do estudante: ", type(estudante))
